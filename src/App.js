6a91d58fac957a32604759fab17812621355b42c
kohoon <ko@crowdbase.kr>
2020-10-18 23:53:06 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logos" />
-      <h2>Let's develop management system</h2> 
-    </div>
-  );
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '고훈',
+  'birthday' : '830326',
+  'gender' : '남성',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '820226',
+  'gender' : '남성',
+  'job' : '프로그래머'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '고은준',
+  'birthday' : '161205',
+  'gender' : '남성',
+  'job' : '어린이'
+}]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="gray-background">
+//       <img src={logo} alt="logos" />
+//       <h2>Let's develop management system</h2> 
+//     </div>
+//   );
+// }
 
 export default App;
